9a36cac46e1edbc2a7a1dd4b3ccaf0a3b9e491ad
Cerealant <92551470+jonny0608@users.noreply.github.com>
2026-09-25 16:28:55 +0000

    added comments

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const programItems = document.querySelectorAll(".program-item");
 
 filterButtons.forEach(button => {
   button.addEventListener("click", () => {
-    filterButtons.forEach(btn => btn.classList.remove("active"));
-    button.classList.add("active");
+    filterButtons.forEach(btn => btn.classList.remove("active")); // removes active class from all buttons on click (removes red color from program button)
+    button.classList.add("active");// adds active class to the clicked button (adds red color to program button)
 
-    const filter = button.dataset.filter;
+    const filter = button.dataset.filter; // grabs value from html of dataset filter 
 
     programItems.forEach(item => {
       item.classList.toggle(
         "hidden-program",
-        filter !== "all" && filter !== item.dataset.category
+        filter !== "all" && filter !== item.dataset.category // adds hidden program to all of the program elements BESIDES the one that was clicked on
       );
     });
   });
@@ -19,7 +19,7 @@ filterButtons.forEach(button => {
 
 document.querySelectorAll(".mobile-link").forEach(link => {
   link.addEventListener("click", () => {
-    const menu = bootstrap.Offcanvas.getInstance(
+    const menu = bootstrap.Offcanvas.getInstance( 
       document.getElementById("mobileMenu")
     );
 
